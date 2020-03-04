9c4730cc3d94b82ab6bce24507f26c1dfdfed9bd
Chase Granberry <chase@logflare.app>
2020-03-04 12:29:39 -0700

    Move server side

--- pages/api/date.js
+++ pages/api/date.js
@@ -1,4 +1,9 @@
+const logger = require('pino')()
+
 export default (req, res) => {
+
+  logger.info({user: {name: "Joe Schmo", email: "joe@dunder.dev", company: "Dunder Dev", id: 38}, event: {type: "request", tag: "api"}})
+
   const date = new Date()
     .toISOString()
     .replace(/T/, ' ')
