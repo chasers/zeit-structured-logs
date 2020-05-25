dc17907778310f7d4e998c7d3581e34f9df4d5a0
chasers <chase@logflare.app>
2020-05-25 09:52:44 -0700

    Remove console.log

--- pages/api/date.js
+++ pages/api/date.js
@@ -1,14 +1,12 @@
 const logger = require('pino')({
   base: {
-    env: process.env.ENV || "undefined"
+    env: process.env.ENV || "ENV not set"
   }
 })
 
 export default (req, res) => {
 
   logger.info({ user: { name: "Joe Schmo", email: "joe@dunder.dev", company: "Dunder Dev", id: 38 }, event: { type: "request", tag: "api" } })
-
-  console.log(process.env.ENV)
 
   const date = new Date()
     .toISOString()
