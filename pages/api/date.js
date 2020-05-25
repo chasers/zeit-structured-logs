73957f3ebc6b039fcb4c4ff8af04dfda0139d3c2
chasers <chase@logflare.app>
2020-05-25 09:47:42 -0700

    Touch

--- pages/api/date.js
+++ pages/api/date.js
@@ -1,12 +1,14 @@
 const logger = require('pino')({
   base: {
-    env: process.env.ENV || "development"
+    env: process.env.ENV || "undefined"
   }
 })
 
 export default (req, res) => {
 
   logger.info({ user: { name: "Joe Schmo", email: "joe@dunder.dev", company: "Dunder Dev", id: 38 }, event: { type: "request", tag: "api" } })
+
+  console.log(process.env.ENV)
 
   const date = new Date()
     .toISOString()
