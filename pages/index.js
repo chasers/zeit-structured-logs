a4d03807c77632f26c041fa7e64a3566fc74ffc0
chasers <chase@logflare.app>
2020-05-25 14:25:50 -0700

    Don't use Link w/ external links

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import Link from 'next/link';
 import Head from '../components/head';
 import Nav from '../components/nav';
 
-const Home = () => {
+const Home = props => {
+  console.log(props.headers)
   const [date, setDate] = useState(null);
 
   useEffect(() => {
@@ -39,24 +40,18 @@ const Home = () => {
         </p>
 
         <div className="row">
-          <Link href="https://github.com/zeit/next.js#setup">
-            <a className="card">
-              <h3>Getting Started &rarr;</h3>
-              <p>Learn more about Next.js on GitHub and in their examples.</p>
-            </a>
-          </Link>
-          <Link href="https://github.com/zeit/next.js/tree/master/examples">
-            <a className="card">
-              <h3>Examples &rarr;</h3>
-              <p>Find other example boilerplates on the Next.js GitHub.</p>
-            </a>
-          </Link>
-          <Link href="https://github.com/zeit/next.js">
-            <a className="card">
-              <h3>Create Next App &rarr;</h3>
-              <p>Was this tool helpful? Let us know how we can improve it!</p>
-            </a>
-          </Link>
+          <a className="card" href="https://github.com/zeit/next.js#setup">
+            <h3>Getting Started &rarr;</h3>
+            <p>Learn more about Next.js on GitHub and in their examples.</p>
+          </a>
+          <a className="card" href="https://github.com/zeit/next.js/tree/master/examples">
+            <h3>Examples &rarr;</h3>
+            <p>Find other example boilerplates on the Next.js GitHub.</p>
+          </a>
+          <a className="card" href="https://github.com/zeit/next.js">
+            <h3>Create Next App &rarr;</h3>
+            <p>Was this tool helpful? Let us know how we can improve it!</p>
+          </a>
         </div>
       </div>
 
