4ee354831429bcbc673846a3eac551e7b4fcc656
chasers <chase@logflare.app>
2020-05-27 06:47:00 -0700

    Revert

--- logger/logflareStream.js
+++ logger/logflareStream.js
@@ -1,8 +1,8 @@
 import { createWriteStream } from 'pino-logflare';
 
 export default createWriteStream({
-    apiKey: process.env.LOGFLARE_INGEST_API_KEY || "S85LoAXJUB8U",
-    source: process.env.LOGFLARE_SOURCE_ID || "d90cce76-de32-41cd-91a7-c1a74fc1eae0",
-    sourceToken: process.env.LOGFLARE_SOURCE_ID || "d90cce76-de32-41cd-91a7-c1a74fc1eae0",
+    apiKey: "S85LoAXJUB8U",
+    source: "d90cce76-de32-41cd-91a7-c1a74fc1eae0",
+    sourceToken: "d90cce76-de32-41cd-91a7-c1a74fc1eae0",
     batchMaxSize: 1,
 });
