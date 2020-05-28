15e8351e1e28979f8352d30d7092422cb71a5c0a
chasers <chase@logflare.app>
2020-05-28 13:08:30 -0700

    Revert console logging

--- pages/api/date.js
+++ pages/api/date.js
@@ -6,7 +6,6 @@ export default (req, res) => {
   const headers = prepObjectKeys(req.headers)
 
   logger.info({ request: { headers: headers } }, "API request")
-  console.log(JSON.stringify({ environment: process.env.ENV }))
 
   const date = new Date()
     .toISOString()
